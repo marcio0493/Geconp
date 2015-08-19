327e25602988880af2c826caf663e8bc812884d3
marcio0493 <marciomndsilva@gmail.com>
2015-08-19 18:20:54 -0300

    Create AttMovDespesaActivity.java

---
+++ app/src/main/java/br/com/geconp/geconp/AttMovDespesaActivity.java
@@ -0,0 +1,258 @@
+package br.com.geconp.geconp;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.Calendar;
+import java.util.List;
+
+import br.com.geconp.geconp.async.AsyncAtMovDes;
+import br.com.geconp.geconp.dao.DatabaseManager;
+import br.com.geconp.geconp.model.Despesa;
+import br.com.geconp.geconp.model.MovimentoDespesa;
+
+
+public class AttMovDespesaActivity extends Activity {
+    private ProgressDialog carregando;
+    private String itemTipo, itemConta;
+    private Spinner spCat;
+    private Spinner spCon;
+    private EditText txtData, txtValor, txtDescricao;
+    static final int DATE_DIALOG_ID = 0;
+    private String data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_att_mov_despesa);
+
+        spCat= (Spinner)findViewById(R.id.spinnerAttTipoMoDes);
+        spCon= (Spinner) findViewById(R.id.spinnerAttConMoDes);
+
+        loadSpinnerData();
+        loadSpinnerConta();
+
+        itemSelecionado(spCat, spCon);
+        itemTipo= spCat.getSelectedItem().toString();
+        //colocar do outro spinner
+        itemConta= spCon.getSelectedItem().toString();
+
+
+        txtData = (EditText) findViewById(R.id.editTextAttDatMovDes);
+        txtValor= (EditText)findViewById(R.id.editTextAttValMovDes);
+        txtDescricao= (EditText)findViewById(R.id.editTextAttDesMoDes);
+
+        Intent origem= getIntent();
+        final String idRec= origem.getStringExtra("ID");
+        String valorRcebido= origem.getStringExtra("VALOR");
+        String dataRecebido= origem.getStringExtra("DATA");
+        String descRecebido= origem.getStringExtra("DESCRICAO");
+        txtValor.setText(valorRcebido);
+        txtData.setText(dataRecebido);
+        txtDescricao.setText(descRecebido);
+
+        Button add= (Button)findViewById(R.id.buttonAttMovDes);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gravar(idRec, itemTipo, itemConta);
+            }
+        });
+
+    }
+
+    public void itemSelecionado(final Spinner spinner, final Spinner spinnerConta){
+        Log.i("TAG", "Selecionado");
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                itemTipo = parent.getItemAtPosition(position).toString();
+                Log.i("TAG", "pegar valor Tipo" + itemTipo);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                return;
+            }
+        });
+
+        spinnerConta.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                itemConta = parent.getItemAtPosition(position).toString();
+                Log.i("TAG", "pegar valor Conta" + itemConta);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    public void loadSpinnerData(){
+        DatabaseManager manager= new DatabaseManager(getApplicationContext());
+        List<String> labels= manager.listarCategoriasDespesa();
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, labels);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spCat.setAdapter(adapter);
+
+    }
+
+    public void loadSpinnerConta(){
+        DatabaseManager manager= new DatabaseManager(getApplicationContext());
+        List<String> labels= manager.listarConta();
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, labels);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spCon.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        txtData = (EditText) findViewById(R.id.editTextAttDatMovDes);
+        txtData.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                showDialog(DATE_DIALOG_ID);
+                txtData.clearFocus();
+            }
+        });
+
+    }
+
+    private void gravar(String idRec, final String itemTipo, final String itemConta) {
+        if (txtValor.getText().toString().equals(".00") || txtValor.getText().toString().equals("") ||
+                txtData.getText().toString().equals("") || txtDescricao.getText().toString().equals("")) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setTitle(R.string.cuidado);
+            dialog.setIcon(R.drawable.ic_action_warning);
+            dialog.setMessage(R.string.campoVazio);
+            dialog.setNeutralButton("Ok", null);
+            dialog.show();
+        } else {
+            //Bloco de formatação do valor
+            String aux1 = txtValor.getText().toString();
+            String aux3 = aux1.replace(",", ".");
+            float valor = Float.parseFloat(aux3);
+
+            String d = txtData.getText().toString();
+            Log.i("INFO3", "Data Chegou aqui= " + d);
+            String descricao = txtDescricao.getText().toString();
+
+            long id = Long.parseLong(idRec);
+
+            MovimentoDespesa movimento= new MovimentoDespesa(id, valor, d, descricao, itemTipo, itemConta);
+            //new AsyncAtu(movimento).execute();
+            DatabaseManager manager= new DatabaseManager(AttMovDespesaActivity.this);
+            manager.atualizarMovDespesa(movimento);
+
+            txtValor.setText("");
+            txtData.setText("");
+            txtDescricao.setText("");
+
+            AlertDialog.Builder dialog= new AlertDialog.Builder(this);
+            dialog.setTitle("Sucesso!");
+            dialog.setIcon(R.drawable.ic_action_refresh);
+            dialog.setMessage(R.string.mensagem_atualizar);
+            dialog.setNeutralButton("Ok", null);
+            dialog.show();
+
+            setResult(1, new Intent());
+            finish();
+        }
+    }
+    @Override
+    @Deprecated
+    protected Dialog onCreateDialog(int id) {
+        Calendar calendar= Calendar.getInstance();
+        int ano = calendar.get(Calendar.YEAR);
+        int mes = calendar.get(Calendar.MONTH);
+        int dia = calendar.get(Calendar.DAY_OF_MONTH);
+        switch (id) {
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this, mDateSetListener, ano, mes, dia);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            data = String.valueOf(dayOfMonth) + "/" + String.valueOf(monthOfYear+1) + "/"
+                    + String.valueOf(year);
+            txtData.setText(data);
+            Log.i("INFO", "DATA = " + data);
+        }
+    };
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_att_mov_despesa, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class AsyncAtu extends AsyncTask<Void, Void, Void>{
+        MovimentoDespesa movimento;
+
+        private AsyncAtu(MovimentoDespesa movimento) {
+            this.movimento = movimento;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            DatabaseManager manager= new DatabaseManager(AttMovDespesaActivity.this);
+            manager.atualizarMovDespesa(this.movimento);
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            carregando= new ProgressDialog(AttMovDespesaActivity.this);
+            carregando.setTitle("Aguarde");
+            carregando.setMessage("Atualizando dados...");
+            carregando.show();
+
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            carregando.dismiss();
+        }
+    }
+}
